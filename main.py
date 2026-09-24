44b1b21878a3aad34ea9a38d41137a0c9a6c979f
rohith-dev33 <rohithgowda4568@gmail.com>
2026-09-24 11:31:49 +0530

    Update database setup and gitignore

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+from database import Base, engine
+import models
+
 app = FastAPI(title="AI JOB TRACKER")
+
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
 def home():
-    return{"message": "AI JOB TRACKER"}
+    return {"message": "AI JOB TRACKER"}
